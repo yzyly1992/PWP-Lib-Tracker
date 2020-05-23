1c7daec1463eda192e7ebbbb4978e2d37a3a03ea
David Yang <zyang03@risd.edu>
2020-05-22 18:39:59 -0700

    Revise Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-    paths = ["/mnt/jarvis/Library/PWP-LIBRARY/CUTOUTS/PLANTS/ALL\ PLANTS\ Collection/", "/mnt/jarvis/Library/PWP-LIBRARY/CUTOUTS/PEOPLE/PHOTO\ PEOPLE/"]
+    paths = ["/mnt/jarvis/Library/PWP-LIBRARY/CUTOUTS/PLANTS/ALL PLANTS Collection", "/mnt/jarvis/Library/PWP-LIBRARY/CUTOUTS/PEOPLE/PHOTO PEOPLE"]
     observers = []
     event_handler = PatternMatchingEventHandler(patterns=["*.png"], ignore_patterns = "", ignore_directories=True, case_sensitive=True)
     publicPath = "/home/dyang/PWP-Lib-Search/build"
@@ -32,96 +32,106 @@ if __name__ == "__main__":
         folderName = filePath.splite("/")[5]
         
         if folderName == "PLANTS":
-            if filePath[-3:] == "png":
-                try:
-                    im = Image.open(filePath)
-                except IOError:
-                    print("failed to identify", path)
-                name = filePath.splite("/")[-1][:-4]
-                if re.match(r'.*?tree.*?', name, flags=re.IGNORECASE):
-                    category = "Tree"
-                    name = name.replace('Tree', '')
-                elif re.match(r'.*?shrub.*?', name, flags=re.IGNORECASE):
-                    category = "Shrub"
-                    name = name.replace('Shrub', '')
-                elif re.match(r'.*?flower.*?', name, flags=re.IGNORECASE):
-                    category = "Flower"
-                    name = name.replace('Flower', '')
-                elif re.match(r'.*?grass.*?', name, flags=re.IGNORECASE):
-                    category = "Grass"
-                    name = name.replace('Grass', '')
-                elif re.match(r'.*?groundcover.*?', name, flags=re.IGNORECASE):
-                    category = "Groundcover"
-                    name = name.replace('Groundcover', '')
-                elif re.match(r'.*?aquatic.*?', name, flags=re.IGNORECASE):
-                    category = "Aquatic"
-                    name = name.replace('Aquatic', '')
-                elif re.match(r'.*?succulent.*?', name, flags=re.IGNORECASE):
-                    category = "Succulent"
-                    name = name.replace('Succulent', '')
-                else:
-                    category = "Other"
+            try:
+                im = Image.open(filePath)
+            except IOError:
+                print("failed to identify", path)
+            
+            name = filePath.splite("/")[-1][:-4]
+            if re.match(r'.*?tree.*?', name, flags=re.IGNORECASE):
+                category = "Tree"
+                name = name.replace('Tree', '')
+            elif re.match(r'.*?shrub.*?', name, flags=re.IGNORECASE):
+                category = "Shrub"
+                name = name.replace('Shrub', '')
+            elif re.match(r'.*?flower.*?', name, flags=re.IGNORECASE):
+                category = "Flower"
+                name = name.replace('Flower', '')
+            elif re.match(r'.*?grass.*?', name, flags=re.IGNORECASE):
+                category = "Grass"
+                name = name.replace('Grass', '')
+            elif re.match(r'.*?groundcover.*?', name, flags=re.IGNORECASE):
+                category = "Groundcover"
+                name = name.replace('Groundcover', '')
+            elif re.match(r'.*?aquatic.*?', name, flags=re.IGNORECASE):
+                category = "Aquatic"
+                name = name.replace('Aquatic', '')
+            elif re.match(r'.*?succulent.*?', name, flags=re.IGNORECASE):
+                category = "Succulent"
+                name = name.replace('Succulent', '')
+            else:
+                category = "Other"
 
-                with open('/home/dyang/PWP-Lib-Search/public/plants.json', 'r') as plantJson:
-                    plantData = json.load(plantJson)
+            with open('/home/dyang/PWP-Lib-Search/public/plants.json', 'r') as plantJson:
+                plantData = json.load(plantJson)
 
-                thumbDir = "/Thumbnails/"
-                idNum = plantData[-1]["id"] + 1
-                imageSize = im.size
-                name = name.replace('-', ' ').replace('_', ' ').replace('+', ' ')
-                fullName = " ".join([i for i in name.split() if not i.isdigit()])
-                dataType = "2D Cutout"
-                fileSize = convert_size(os.path.getsize(filePath))
-                createTime = time.ctime(os.path.getmtime(filePath))
-                thumbName150 = fullName + " " + str(idNum) + " 150p.jpg"
-                thumbName300 = fullName + " " + str(idNum) + " 300p.jpg"
-                thumb150path = os.path.join(thumbDir, thumbName150)
-                thumb300path = os.path.join(thumbDir, thumbName300)
-                thumbName150Png = fullName + " " + str(idNum) + " 150p.png"
-                thumbName300Png = fullName + " " + str(idNum) + " 300p.png"
-                thumb150pathPng = os.path.join(thumbDir, thumbName150Png)
-                thumb300pathPng = os.path.join(thumbDir, thumbName300Png)
+            thumbDir = "/Thumbnails/"
+            idNum = plantData[-1]["id"] + 1
+            imageSize = im.size
+            name = name.replace('-', ' ').replace('_', ' ').replace('+', ' ')
+            fullName = " ".join([i for i in name.split() if not i.isdigit()])
+            dataType = "2D Cutout"
+            fileSize = convert_size(os.path.getsize(filePath))
+            createTime = time.ctime(os.path.getmtime(filePath))
+            thumbName150 = fullName + " " + str(idNum) + " 150p.jpg"
+            thumbName300 = fullName + " " + str(idNum) + " 300p.jpg"
+            thumb150path = os.path.join(thumbDir, thumbName150)
+            thumb300path = os.path.join(thumbDir, thumbName300)
+            thumbName150Png = fullName + " " + str(idNum) + " 150p.png"
+            thumbName300Png = fullName + " " + str(idNum) + " 300p.png"
+            thumb150pathPng = os.path.join(thumbDir, thumbName150Png)
+            thumb300pathPng = os.path.join(thumbDir, thumbName300Png)
+            mac = filePath.replace("/mnt/jarvis", "/Volumes")
+            filePath = filePath.replace("/mnt/jarvis/Library", "Y:").replace("/", "\\")
 
-                if im.mode == "RGBA":
-                    bg = Image.new("RGB", im.size, (255, 255, 255))
-                    bg.paste(im, mask=im)
-                    im.save(os.path.join(publicPath, thumb300path))
-                    im.save(os.path.join(buildPath, thumb300path))
-                    bg.save(thumb300path)
-                    im.save(os.path.join(publicPath, thumb150path))
-                    im.save(os.path.join(buildPath, thumb150path))
-                    bg.save(thumb150path)
-                    return plantData.append({
-                        "id": idNum,
-                        "name": fullName,
-                        "category": category,
-                        "path": filePath, 
-                        "dataType": dataType, 
-                        "fileSize": fileSize,
-                        "createTime": createTime,
-                        "imageSize": imageSize,
-                        "thumb150path": thumb150path,
-                        "thumb300path": thumb300path})
-                else:
-                    im.thumbnail((300,300), Image.ANTIALIAS)
-                    im.save(os.path.join(publicPath, thumb300pathPng), "PNG")
-                    im.save(os.path.join(buildPath, thumb300pathPng), "PNG")
-                    im.thumbnail((150,150),  Image.ANTIALIAS)
-                    im.save(os.path.join(publicPath, thumb150pathPng), "PNG")
-                    im.save(os.path.join(buildPath, thumb150pathPng), "PNG")
-                    return plantData.append({
-                        "id": idNum,
-                        "name": fullName,
-                        "category": category,
-                        "path": filePath, 
-                        "dataType": dataType, 
-                        "fileSize": fileSize,
-                        "createTime": createTime,
-                        "imageSize": imageSize,
-                        "thumb150path": thumb150pathPng,
-                        "thumb300path": thumb300pathPng})
+            if im.mode == "RGBA":
+                bg = Image.new("RGB", im.size, (255, 255, 255))
+                bg.paste(im, mask=im)
+                im.save(os.path.join(publicPath, thumb300path))
+                im.save(os.path.join(buildPath, thumb300path))
+                bg.save(thumb300path)
+                im.save(os.path.join(publicPath, thumb150path))
+                im.save(os.path.join(buildPath, thumb150path))
+                bg.save(thumb150path)
+                return plantData.append({
+                    "id": idNum,
+                    "name": fullName,
+                    "category": category,
+                    "path": filePath, 
+                    "dataType": dataType, 
+                    "fileSize": fileSize,
+                    "createTime": createTime,
+                    "imageSize": imageSize,
+                    "thumb150path": thumb150path,
+                    "thumb300path": thumb300path,
+                    "mac": mac})
+            else:
+                im.thumbnail((300,300), Image.ANTIALIAS)
+                im.save(os.path.join(publicPath, thumb300pathPng), "PNG")
+                im.save(os.path.join(buildPath, thumb300pathPng), "PNG")
+                im.thumbnail((150,150),  Image.ANTIALIAS)
+                im.save(os.path.join(publicPath, thumb150pathPng), "PNG")
+                im.save(os.path.join(buildPath, thumb150pathPng), "PNG")
+                return plantData.append({
+                    "id": idNum,
+                    "name": fullName,
+                    "category": category,
+                    "path": filePath, 
+                    "dataType": dataType, 
+                    "fileSize": fileSize,
+                    "createTime": createTime,
+                    "imageSize": imageSize,
+                    "thumb150path": thumb150pathPng,
+                    "thumb300path": thumb300pathPng,
+                    "mac": mac})
                 
-                print(plantData[-1])
+            print(plantData[-1])
+
+            with open('/home/dyang/PWP-Lib-Search/public/plants.json', 'w') as publicJson:
+                json.dump(plantData, publicJson)
+            with open('/home/dyang/PWP-Lib-Search/build/plants.json', 'w') as buildJson:
+                json.dump(plantData, buildJson)
+
                 
 
     def on_deleted(event):
@@ -129,17 +139,21 @@ if __name__ == "__main__":
         folderName = filePath.splite("/")[5]
         
         if folderName == "PLANTS":
-            if filePath[-3:] == "png":
-                with open('/home/dyang/PWP-Lib-Search/public/plants.json', 'r') as plantJson:
-                    plantData = json.load(plantJson)
-                for item in plantData:
-                    if item["path"] == filePath:
-                        os.remove(os.path.join(publicPath, item["thumb150path"]))
-                        os.remove(os.path.join(publicPath, item["thumb300path"]))
-                        os.remove(os.path.join(buildPath, item["thumb150path"]))
-                        os.remove(os.path.join(buildPath, item["thumb300path"]))
-                        plantData = plantData.remove(item)
-                print(plantData[-1])
+            with open('/home/dyang/PWP-Lib-Search/public/plants.json', 'r') as plantJson:
+                plantData = json.load(plantJson)
+            for item in plantData:
+                if item["path"] == filePath.replace("/mnt/jarvis/Library", "Y:").replace("/", "\\"):
+                    os.remove(os.path.join(publicPath, item["thumb150path"]))
+                    os.remove(os.path.join(publicPath, item["thumb300path"]))
+                    os.remove(os.path.join(buildPath, item["thumb150path"]))
+                    os.remove(os.path.join(buildPath, item["thumb300path"]))
+                    plantData = plantData.remove(item)
+            
+            print(plantData[-1])
+            with open('/home/dyang/PWP-Lib-Search/public/plants.json', 'w') as publicJson:
+                json.dump(plantData, publicJson)
+            with open('/home/dyang/PWP-Lib-Search/build/plants.json', 'w') as buildJson:
+                json.dump(plantData, buildJson)
 
 
 
@@ -151,14 +165,15 @@ if __name__ == "__main__":
         observer.schedule(event_handler, path, recursive=False)
         observers.append(observer)
     observer.start()
+    print(observers)
 
     try:
         while True:
-            time.sleep(60)
+            time.sleep(5)
     except KeyboardInterrupt:
-        for observer in observers:
-            observer.unschedule_all()
-            observer.stop()
+        for o in observers:
+            o.unschedule_all()
+            o.stop()
 
-    for observer in observers:
-        observer.join()
+    for o in observers:
+        o.join()
